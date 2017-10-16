6e70f4ca6be59ab7f445584761f91f84b9bc4eb0
Santiago Chávez <sanxofon@gmail.com>
2017-10-16 17:22:36 -0500

    Actualizaciones, correcciones y README

--- argumentosParsed.py
+++ argumentosParsed.py
@@ -5,8 +5,11 @@
 	Una manera más eficaz para capurar varios argumentos con beneficios añadidos
 	es usar la librería especial para ese fin: "argparse"
 	Ejemplos de ejecución:
-	>> python argumentosParsed.py -n "Santiago Chávez"
-	>> python argumentosParsed.py -n "Santiago Chávez" -e
+	>> python argumentosParsed.py -a "Santiago Chávez"
+	>> python argumentosParsed.py -a "Santiago Chávez" -m
+	>> python argumentosParsed.py -a "Santiago Chávez" -M
+	Este ejemplo debe mostrar error de codificación en Windows
+	>> python argumentosParsed.py -a "Santiago Chávez" -M > test,txt
 """
 
 # Importamos la librería "argparse" que nos permite acceder de forma ordenada a los argumentos
@@ -19,22 +22,27 @@ import sys
 # Inicializamos el "parser" de argumentos con la descripción general
 parser = argparse.ArgumentParser(description=u'Este programa muestra como recibir datos del usuario como argumentos\nEjemplo: python argumentosParsed.py -n "Santiago Chávez"')
 
-# Define la "bandera" de nombre (corta: "-n", larga: "-name") y su texto de ayuda (help=""). Es un argumento obligatorio (required=True)
-parser.add_argument("-n", "--name", help=u"Nombre del usuario", required=True)
+# Define la "bandera" de nombre (corta: "-a", larga: "-arg") y su texto de ayuda (help=""). Es un argumento obligatorio (required=True)
+parser.add_argument("-a", "--arg", help=u"Argumento, acepta una cadena de texto", required=True)
 
-# Define la bandera booleana extra (corta: "-e", larga: "-extra"). BOOLEANO: no recibe valores extra (action="store_true")
-parser.add_argument("-e", "--extra", help=u"Extra: Booleano, no recibe valor", action="store_true")
+# Define la bandera booleana de convertir a minúsculas (corta: "-m", larga: "-min"). BOOLEANO: no recibe valores extra (action="store_true")
+parser.add_argument("-m", "--min", help=u"Convertir a minúsculas: Booleano, no recibe valor", action="store_true")
+
+# Define la bandera booleana de convertir a MAYÚSCULAS (corta: "-M", larga: "-may"). BOOLEANO: no recibe valores extra (action="store_true")
+parser.add_argument("-M", "--may", help=u"Convertir a MAYÚSCULAS: Booleano, no recibe valor", action="store_true")
 
 # Finalizamos el proceso de parceo, guardamos los argumentos en la variable "args"
 args = parser.parse_args()
 
-name = args.name.decode(sys.stdin.encoding)
-
-extra = "No"
-if args.extra:
-	extra = "Si"
+# Codificación (encoding) del sistema (terminal)
+cds = sys.stdin.encoding
+print u"Encoding:",cds
+arg = args.arg.decode(cds)
 
 # Por último imprimimos el argumento "name" recibido
 print u"Argumentos recibidos:"
-print "\tNombre: ",name
-print "\tExtra: ",extra
+print u"\tArgumento:",arg
+if args.min:
+	print u"\tminúsculas:",arg.lower()
+if args.may:
+	print u"\tMAYÚSCULAS:",arg.upper()
--- vacio.py
+++ vacio.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+# Este es un comentario de una sola línea
+
+"""
+	Este es un comentario
+	de varias líneas
+	...
+"""
