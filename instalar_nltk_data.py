56fc971287788c9e0257ee190b51dc6e49cd57a5
Santiago Chávez <sanxofon@gmail.com>
2017-10-18 02:20:25 -0500

    BasciNLTK en proceso, instalador de pip y lista de paquetes nltk

---
+++ instalar_nltk_data.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import nltk
+
+nltk.download("cess_esp")
+nltk.download("spanish_grammars")
+nltk.download("crubadan")
+nltk.download("conll2002")
+nltk.download("omw")
+nltk.download("stopwords")
+nltk.download("swadesh")
+nltk.download("universal_tagset")
+nltk.download("universal_treebanks_v20")
+nltk.download("words")
