d720a965664289c8c9d73eb5c009744ef54b26b1
Santiago Chávez <sanxofon@gmail.com>
2017-11-12 00:19:56 -0600

    mediumRegex

--- mediumRegexUTF8.py
+++ mediumRegexUTF8.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# FIX PARA WINDOWS CONSOLE ----------------------
+import codecs,sys
+sys.stdout = codecs.getwriter("utf8")(sys.stdout)
+# -----------------------------------------------
+
 import re
-cadena = u"—¡Joven «emponzoñado» con el whisky, qué fin… te aguarda exhibir!"
+cadena = u"""—¡Joven «emponzoñado» con el whisky, qué fin… te aguarda exhibir!
+El pingüino Wenceslao hizo kilómetros bajo exhaustiva 
+lluvia y frío, añoraba a su querido cachorro."""
 patrones = [
-	ur"([\w]+)",
-	ur"([\w]+) ([\w]+)",
-	ur"([^ ]+) ([^ ]+)",
-	ur"([\w]+)[^\w]? [^\w]?([\w]+)",
+	(ur"(\w)", u"Busca todos los caracteres de palabra"),
+	(ur"(\W)", u"Busca todos los caracteres que no son de palabra"),
+	(ur"(\s)", u"Busca todos los caracteres de espaciado"),
+	(ur"(\S)", u"Busca todos los caracteres que no son de espaciado"),
+	(ur"(\w+)", u"Busca todas las palabras"),
+	(ur"(\w+)\s+(\w+)", u"Busca pares de palabras separadas por un espacio"),
+	(ur"([^\s]+)\s+([^\s]+)", u"Busca dos grupos de caracteres que no sean espacios seguidos, separados por un espacio"),
+	(ur"(\w+)[^\w\s]?\s+[^\w\s]?(\w+)", u"Busca dos palabras separadas por un espacio que pueden o no tener un caractes no de palabra a los lados"),
+	(ur"(\w+)\s+(?=(\w+))","Busca todos los pares de palabras (separadas por espacio) con lookahead"),
+	(ur"(\w+)(?=(?:\s+(\W*)(\w+))|([^\w\r\n]+))","Busca pares de palabra/palabra o palabra/otro, puede incluir caracteres que anteceden la segunda palabra"),
 ]
 print u"\nCadena:",cadena
-for patron in patrones:
-	paco = re.compile(patron, re.UNICODE)
+for i,patron in enumerate(patrones):
+	if (len(sys.argv)>1 and sys.argv[1]!=str(i)):
+		continue
+	paco = re.compile(patron[0], re.UNICODE)
 	match = paco.findall(cadena)
-	print u"\nPatrón:",patron
+	print "\n",patron[1]
+	print "\t",patron[0],"\n"
 	if len(match)>0:
-		if isinstance(match[0], tuple):
-			for m in match:
-				print "\t", "\t".join(m)
-		else:
-			print  "\t", "\n\t".join(match)
+		for ii,m in enumerate(match):
+			if isinstance(match[0], tuple):
+				m = filter(None, m) # Elimina los valores vacíos
+				print ii,"\t", "\t".join(m)
+			else:
+				print ii,"\t", m
 	else:
 		print "\tNo hubo coincidencias"
 print
