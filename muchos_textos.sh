6492a2372458fdb4f089478c1c893f2b3e43b332
Santiago Chávez <sanxofon@gmail.com>
2017-12-14 08:29:22 -0600

    muchos textos

---
+++ muchos_textos.sh
@@ -0,0 +1,10 @@
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Angie Sage/Septimus.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Del Rey, Lester/El dios más pequeño y otros relatos.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Vandenberg/El secreto de los oráculos.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Sheldon, Sydney/Escrito en las Estrellas.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Winston Groom/Forrest Gump.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Winston Groom/Gump & Co.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/Styles, Michelle/Siete Días Sin Besos.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/George R. R. Martin/El caballero errante.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/George R. R. Martin/Tormenta de Espadas.txt" -i 9 >> muchos_textos_2gramx.csv
+python txtFileRegEx.py -f "/home/jaci/git/EPUB/George R. R. Martin/Juego de Tronos.txt" -i 9 >> muchos_textos_2gramx.csv
